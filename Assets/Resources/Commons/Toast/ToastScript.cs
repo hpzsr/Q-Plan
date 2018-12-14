b8670574d005facd43639a2ce2ad1eb3a8ff805d
hpzsr <582254172@qq.com>
2018-12-14 17:18:59 +0800

    11

---
+++ Assets/Resources/Commons/Toast/ToastScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using DG.Tweening;
+
+public class ToastScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public static GameObject show(string text)
+    {
+        GameObject prefab = Resources.Load("Commons/Toast/Toast") as GameObject;
+        GameObject obj = MonoBehaviour.Instantiate(prefab, GameObject.Find("Canvas").transform);
+        obj.transform.Find("Text").GetComponent<Text>().text = text;
+        
+        obj.GetComponent<RectTransform>().sizeDelta = new Vector2(text.Length * 35, 60);
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(obj.GetComponent<Image>().rectTransform.DOAnchorPos(new Vector2(0, 250), 1.0f))
+           .Append(obj.GetComponent<Image>().rectTransform.DOAnchorPos(new Vector3(0, 250),0.7f)).SetLoops(0).Play().OnComplete(()=>
+           {
+               GameObject.Destroy(obj);
+           });
+
+        return obj;
+    }
+}
---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.Find("Button_start").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            ToastScript.show("暂未开放");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
